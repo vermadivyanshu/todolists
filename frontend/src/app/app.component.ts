f4c4fcbdeae1fc42c0c3b2f5c1ac2258f4871200
Divyanshu Verma <divyanshu.verma1993@gmail.com>
2024-03-04 12:43:03 -0500

    adds logout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { LocalstorageService } from './services/localstorage.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, HttpClientModule],
+  imports: [RouterOutlet, MatToolbarModule, HttpClientModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Todo Lists';
+
+  constructor(private localstorageService: LocalstorageService, private router: Router) {}
+
+  onClickLogout() {
+    this.localstorageService.removeItem('todo-token');
+    this.router.navigate(['login']);
+  }
 }
